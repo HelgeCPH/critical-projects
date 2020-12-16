898ba681b2155acd5da0275f4614eb904b0bffa6
HelgeCPH <ropf@itu.dk>
2020-12-17 00:15:21 +0100

    Fixed missing semicolons to end clauses

--- critical_projects/create_neo4j_pr_comp.py
+++ critical_projects/create_neo4j_pr_comp.py
@@ -5,23 +5,16 @@ if __name__ == "__main__":
     for platform in INCLUDED_PLATFORMS:
         cql = (
             f'CALL gds.graph.create("{platform.lower()}_pr_graph",'
-            + f' "{platform}", "*")'
+            + f' "{platform}", "*");'
         )
         print(cql)
     print()
 
     for platform in INCLUDED_PLATFORMS:
-
-        # cql = (
-        #     f'CALL gds.pageRank.stream("{platform.lower()}_pr_graph")\n'
-        #     + "YIELD nodeId, score\n"
-        #     + "RETURN gds.util.asNode(nodeId).Name AS name, score\n"
-        #     + "ORDER BY score DESC, name ASC\n"
-        # )
         cql = (
             f'CALL gds.pageRank.write("{platform.lower()}_pr_graph", {{'
-            + "maxIterations: 20, dampingFactor: 0.85,"
-            + 'writeProperty: "pagerank"})\n'
-            + "YIELD nodePropertiesWritten, ranIterations"
+            + "maxIterations: 20, dampingFactor: 0.85,"  # Default values
+            + 'writeProperty: "pagerank"}) '
+            + "YIELD nodePropertiesWritten, ranIterations;"
         )
         print(cql)
